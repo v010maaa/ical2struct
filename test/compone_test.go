1c497ed961ac3254639e61a3f4ec8e8d602ddc91
morimori <v010maaa@gmail.com>
2022-02-23 11:36:51 +0900

    test: create test script

---
+++ test/compone_test.go
@@ -0,0 +1,75 @@
+package test
+
+import (
+	"fmt"
+	"ical2json/ics"
+	"io/ioutil"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNomalParse(t *testing.T) {
+	bytes, _ := ioutil.ReadFile("data/normal.ics")
+	expected := string(bytes)
+	cal := ics.ParseIcs(expected)
+	actual := ics.ComposeIcs(cal.Events)
+
+	if diff := cmp.Diff(actual, expected); diff != "" {
+		t.Errorf("Hogefunc differs: (-got +want)\n%s", diff)
+	}
+
+}
+
+func TestAirBnbParse(t *testing.T) {
+	input_bytes, _ := ioutil.ReadFile("data/airbnb.ics")
+	expected_bytes, _ := ioutil.ReadFile("data/airbnb_expected.ics")
+
+	input := string(input_bytes)
+	expected := string(expected_bytes)
+
+	cal := ics.ParseIcs(input)
+	actual := ics.ComposeIcs(cal.Events)
+
+	if diff := cmp.Diff(actual, expected); diff != "" {
+		t.Errorf("Hogefunc differs: (-got +want)\n%s", diff)
+	}
+}
+
+func TestAlarmParse(t *testing.T) {
+	input_bytes, _ := ioutil.ReadFile("data/alarm.ics")
+	expected_bytes, _ := ioutil.ReadFile("data/alarm_expected.ics")
+
+	input := string(input_bytes)
+	expected := string(expected_bytes)
+
+	cal := ics.ParseIcs(input)
+	actual := ics.ComposeIcs(cal.Events)
+	if diff := cmp.Diff(actual, expected); diff != "" {
+		t.Errorf("Hogefunc differs: (-got +want)\n%s", diff)
+	}
+}
+
+func TestNoEndCompose(t *testing.T) {
+	event_list := NoEndEvents()
+	actual := ics.ComposeIcs(event_list)
+
+	expected_bytes, _ := ioutil.ReadFile("data/no_end_expected.ics")
+
+	expected := string(expected_bytes)
+	if diff := cmp.Diff(actual, expected); diff != "" {
+		t.Errorf("Hogefunc differs: (-got +want)\n%s", diff)
+	}
+}
+
+func TestRandomCompose(t *testing.T) {
+	event_list := RamdomEvents()
+	actual := ics.ComposeIcs(event_list)
+
+	expected_bytes, _ := ioutil.ReadFile("data/ramdom_expected.ics")
+	expected := string(expected_bytes)
+	fmt.Println(actual)
+	if diff := cmp.Diff(actual, expected); diff != "" {
+		t.Errorf("Hogefunc differs: (-got +want)\n%s", diff)
+	}
+}
